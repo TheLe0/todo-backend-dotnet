fba09f5b7b5d3c16fc5d77ff7f63eacde6373740
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2023-05-27 16:49:36 -0300

    feat: Configurations for the CosmosDb persistance mode

---
+++ src/Todo.Configuration/CosmosDbConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Configuration;
+
+public class CosmosDbConfigurations
+{
+    public string PartitionKey { get; set; }
+    public string EndpointUri { get; set; }
+    public string PrimaryKey { get; set; }
+    public string DatabaseId { get; set; }
+}
--- src/Todo.Configuration/DatabaseConfiguration.cs
+++ src/Todo.Configuration/DatabaseConfiguration.cs
@@ -1,18 +1,32 @@
-﻿using System;
-namespace Todo.Configuration;
+﻿namespace Todo.Configuration;
 
 public class DatabaseConfiguration : IDatabaseConfiguration
 {
 	private readonly DatabaseType _type;
+	private readonly CosmosDbConfigurations _cosmosConfigurations;
 
 	public DatabaseConfiguration(DatabaseType type)
 	{
 		_type = type;
-	}
+		_cosmosConfigurations = null;
 
-	public DatabaseType GetDatabaseType()
+    }
+
+    public DatabaseConfiguration(DatabaseType type, CosmosDbConfigurations cosmosConfig)
+    {
+        _type = type;
+        _cosmosConfigurations = cosmosConfig;
+
+    }
+
+    public DatabaseType GetDatabaseType()
 	{
 		return _type;
+	}
+
+	public CosmosDbConfigurations GetCosmosDbConfigurations()
+	{
+		return _cosmosConfigurations;
 	}
 }
 
